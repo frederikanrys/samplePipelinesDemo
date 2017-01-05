d86f6e6dacd633d3d1052b6e06a4919d48e7f1ac
Frederik <frederik.anrys@LT022.local>
2017-01-05 23:34:44 +0100

    port 3000

--- server.js
+++ server.js
@@ -14,5 +14,5 @@ app.get('/*', function (req, res) {
 });
 
 app.listen(3000, function () {
-	console.log('Example app listening on port 80!');
+	console.log('Example app listening on port 3000!');
 });
